8df2e032b241afd7bac7bb5ec27dcd8f123d2ba8
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2023-12-05 22:54:54 -0500

    Update firebase.ts

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,17 +1,27 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDz-c-E5H-LTC4jm5h8rIXKTejqJKJtJ1M",
-  authDomain: "millburn-app.firebaseapp.com",
-  projectId: "millburn-app",
-  storageBucket: "millburn-app.appspot.com",
-  messagingSenderId: "998976946479",
-  appId: "1:998976946479:web:88af7b60b436e63d48a1da"
+  apiKey: "AIzaSyB-uBOC5VLZcSn-DajKVv-TE29jmDZOktk",
+
+  authDomain: "mapp-8c0f4.firebaseapp.com",
+
+  projectId: "mapp-8c0f4",
+
+  storageBucket: "mapp-8c0f4.appspot.com",
+
+  messagingSenderId: "442360603702",
+
+  appId: "1:442360603702:web:5e370950b2eda24d536497",
 };
 
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
